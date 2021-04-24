98a2b4b7e399ae8818484727babe8ca29a22fd63
정혜인(Hazel) <hi.jung@dreamus.io>
2021-04-24 16:22:39 +0900

    first commit

---
+++ Assets/Square.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour
+{
+    float movePower = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 movePosition = Vector3.zero;
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            movePosition = Vector3.left;
+        }
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            movePosition = Vector3.right;
+        }
+        else if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            movePosition = Vector3.up;
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            movePosition = Vector3.down;
+        }
+
+        transform.position += movePosition * movePower * Time.deltaTime;
+    }
+}
